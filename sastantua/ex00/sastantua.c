332c7b841b4df4ffa1e7e15c4e15cacd37072dfa
pedrosimao <psimao-d@e1r3p1.42.fr>
2016-09-05 11:26:56 +0200

    updated all

---
+++ sastantua/ex00/sastantua.c
@@ -0,0 +1,53 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   sastantua.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: psimao-d <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2016/09/04 11:58:57 by psimao-d          #+#    #+#             */
+/*   Updated: 2016/09/04 21:33:15 by psimao-d         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+int		is_even(int numero)
+{
+	if( n % 2 == 0)
+		return(1);
+	else
+		return(0);
+}
+
+
+void	um_andar(int size)
+{
+	int		x;
+	int		y;
+	int		andar[];
+
+	
+
+}
+
+void	minha_porta(int size)
+{
+
+}
+
+
+void	sastantua(int size)
+{
+
+}
+
+int		main()
+{
+	sastantua(5);
+}
+
+
